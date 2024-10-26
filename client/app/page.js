34ac62ac711f813db9b6cd308e9a797a862e030c
gitstar-oc <chandankarom07@gmail.com>
2024-10-26 07:36:43 +0530

    Added new images and paths and changed the design of it

--- client/app/page.js
+++ client/app/page.js
@@ -1,17 +1,17 @@
 // app/page.js
 import Navbar from "@/components/Navbar";
 import Content from "@/components/Content/Content";
-import Footer from "@/components/Footer"
+// import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
-    <div className="bg-neutral-100 dark:bg-neutral-800 text-gray-900 dark:text-gray-100">
+    <div className="bg-neutral-300 dark:bg-neutral-800 text-gray-900 dark:text-gray-100">
       <div className="min-h-screen max-w-6xl mx-auto px-4 sm:px-6 lg:px-8"> {/* Adjusted for better responsiveness */}
         <Navbar />
         <Content /> 
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center pb-8">
+        {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center pb-8">
           <Footer />
-        </div>
+        </div> */}
       </div>
     </div>
   );
